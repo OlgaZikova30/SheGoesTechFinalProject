25e3d5a106011d9b8a441e8f85917ee71b01aa19
sandrakrugalauza <sandra.krugalauza@gmail.com>
2021-05-12 19:33:47 +0300

    Small changes in texts and sql file

--- target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-create table IF NOT EXISTS Book (
-                                     id INT NOT NULL,
-                                     title VARCHAR(50) NOT NULL,
-                                     author VARCHAR(20) NOT NULL,
-                                     submission_date DATE
-                                  );
